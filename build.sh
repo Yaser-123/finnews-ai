610df9d77c7ea0f247b8f4c98838ccf8c477e9e6
Yaser-123 <1ammar.yaser@gmail.com>
2025-12-05 16:37:24 +0530

    fix: Use CPU-only dependencies for Render deployment

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 
 set -o errexit
 
-# Install Python dependencies
+# Install Python dependencies (CPU-only versions)
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirements-render.txt
 
 # Download spaCy model
 python -m spacy download en_core_web_sm
